d50590f47c9ab9a2b150b89366f2df4b1a6303cb
bufu2o2 <bufu2o2@gmail.com>
2019-05-07 19:42:58 -0700

    changed table to have an updatedAt field

--- schema_setup.sql
+++ schema_setup.sql
@@ -1,6 +1,7 @@
+drop table if exists oakvm8l16xv3dcnv.burgers;
 USE oakvm8l16xv3dcnv;
 
-CREATE TABLE burgers(
+CREATE TABLE oakvm8l16xv3dcnv.burgers(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     type VARCHAR(30) NOT NULL,
@@ -8,10 +9,11 @@ CREATE TABLE burgers(
     sauce VARCHAR(30) NOT NULL,
     eaten BOOLEAN DEFAULT false,
     createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
 
 
 
-INSERT INTO burgers (name, type, topping, sauce) VALUES ('bob', 'salmon', 'lettuce', 'mayo');
-INSERT INTO burgers (name, type, topping, sauce, eaten) VALUES ('jimmy', 'impossible', 'cheese', 'ketchup', true);
+INSERT INTO oakvm8l16xv3dcnv.burgers (name, type, topping, sauce) VALUES ('bob', 'salmon', 'lettuce', 'mayo');
+INSERT INTO oakvm8l16xv3dcnv.burgers (name, type, topping, sauce, eaten) VALUES ('jimmy', 'impossible', 'cheese', 'ketchup', true);
